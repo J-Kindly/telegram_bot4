69f7f4f0ddd503e4a0400b870c562ffff4b8477d
Jeel Sapkal <progaming8668@gmail.com>
2025-06-19 17:57:22 +0530

    Added .gitignore to ignore venv and cache

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from keep_alive import keep_alive
+
+BOT_TOKEN = "7847908607:AAFq5-mvHEytFaMOcz38QRn89-yyAaayloA"
+
+keep_alive()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("✅ Bot is working!")
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("✅ Bot is running...")
+    app.run_polling()
